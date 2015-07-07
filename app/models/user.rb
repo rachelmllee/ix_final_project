1be052dd8dce523af1a94b02026197357106d4be
rachelmllee <rachel.ml.lee@gmail.com>
2015-07-07 16:03:12 +0200

    Added name functionality to facebook auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,13 +16,27 @@ class User < ActiveRecord::Base
       email
     end
 
+
     def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
       data = access_token.extra.raw_info
       if user = User.where(:email => data.email).first
         user
-      else
-        User.create!(:email => data.email, :password => Devise.friendly_token[0,20])
+      else # Create a user with a stub password. 
+        User.create!(:email => data.email,
+                     :name => data.name,
+                     # :image => data.image,
+                     :password => Devise.friendly_token[0,20])
       end
     end
+
+    def self.new_with_session(params, session)
+        super.tap do |user|
+          if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+            user.email = data["email"]
+            user.name = data["name"]
+            # user.image = data["image"]
+          end
+        end
+      end
 
 end
