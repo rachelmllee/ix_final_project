6dbfe8f52d846df864008b5649717776b8e53554
rachelmllee <rachel.ml.lee@gmail.com>
2015-07-07 15:09:03 +0200

    Added embedded videos and fixed the nav bar

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -12,4 +12,9 @@ module VideoHelper
 		user_signed_in? && (current_user.admin? || current_user.tutor?)
 	end
 
+	def embed(youtube_url)
+	    youtube_id = youtube_url.split("=").last
+	    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+	end
+
 end
