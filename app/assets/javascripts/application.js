f31007ebef6bd688a4e1ab05902b4828dd488bab
ogobet <trmokwena@gmail.com>
2015-07-08 19:45:11 +0200

    changed something I dont remember

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require chosen-jquery
 //= require jquery.image-select
 //= require messages
 //= require twitter/bootstrap
