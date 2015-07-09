c6083d73ad3c24a79b5b280a8cc73e2b6f8ba80b
rachelmllee <rachel.ml.lee@gmail.com>
2015-07-09 21:04:58 +0200

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+  # change to true?
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
@@ -76,7 +77,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.serve_static_assets = true
 
 end
